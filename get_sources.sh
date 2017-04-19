9d611efb019ac22337e34fa191daaed7f3648d00
Tigro <ashejn@gmail.com>
2017-04-19 22:51:56 +0300

    update to 131

--- get_sources.sh
+++ get_sources.sh
@@ -1,8 +1,8 @@
 COOKIE="oraclelicense=accept-securebackup-cookie"
 
-U=121
-B=13
+U=131
+B=11
 
-curl -sLJO -b "$COOKIE" http://download.oracle.com/otn-pub/java/jdk/8u$U-b$B/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u$U-linux-i586.tar.gz
-curl -sLJO -b "$COOKIE" http://download.oracle.com/otn-pub/java/jdk/8u$U-b$B/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u$U-linux-x64.tar.gz
+curl -sLJO -b "$COOKIE" http://download.oracle.com/otn-pub/java/jdk/8u$U-b$B/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u$U-linux-i586.tar.gz
+curl -sLJO -b "$COOKIE" http://download.oracle.com/otn-pub/java/jdk/8u$U-b$B/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u$U-linux-x64.tar.gz
 
